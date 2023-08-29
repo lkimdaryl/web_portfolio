c5007b60d40d4ddc5004b3e7ef9bd9f5575d380e
lkimdaryl <78583414+lkimdaryl@users.noreply.github.com>
2023-08-29 15:37:10 -0700

    styles updated

---
+++ styles/main.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var myImage = document.querySelector('#my-img');
+    myImage.classList.add('in-focus');
+
+    setTimeout(function() {
+        var myName = document.querySelector('#my-name');
+        myName.classList.add('in-focus');
+    }, 500);
+
+    setTimeout(function() {
+        var myTitle = document.querySelector('#title');
+        myTitle.classList.add('in-focus');
+    }, 1000);
+
+    setTimeout(function() {
+        var slogan = document.querySelector('#slogan');
+        slogan.classList.add('in-focus');
+    }, 1500);
+
+
+})
