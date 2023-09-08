12fd6c1e5c541c2ffc66aded7cf694c9691cf3c1
lkimdaryl <78583414+lkimdaryl@users.noreply.github.com>
2023-09-07 23:44:45 -0700

    changes
    removed pages and images
    added sending message functionality
    updated styles

---
+++ main.py
@@ -0,0 +1,71 @@
+from fastapi import FastAPI, Request               # The main FastAPI import and Request Body object
+from fastapi.responses import HTMLResponse      # Used for returning HTML responses (JSON is default)
+from fastapi.staticfiles import StaticFiles     # Used for making static resources available to server
+import uvicorn
+import sendgrid
+import os
+from sendgrid.helpers.mail import *
+from dotenv import load_dotenv
+
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+# Configuration
+app = FastAPI()                              # Specify the "app" that will run the routing
+app.mount("/public", StaticFiles(directory="public"), name="public")
+
+# load_dotenv("api.env")
+# sg = sendgrid.SendGridAPIClient(api_key=os.environ.get('SENDGRID_API_KEY'))
+# print(f'API Key: {os.environ.get("SENDGRID_API_KEY")}')
+#
+# from_email = Email("lkimdaryl@gmail.com")
+# to_email = To("lkimdaryl@gmail.com")
+# subject = "Sending with SendGrid is Fun"
+# content = Content("text/plain", "and easy to do anywhere, even with Python")
+# mail = Mail(from_email, to_email, subject, content)
+#
+# try:
+#     response = sg.send(mail)
+#     print(response.status_code)
+#     print(response.body)
+#     print(response.headers)
+# except Exception as e:
+#     print(e)
+
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+# Example route: return a static HTML page
+@app.get("/", response_class=HTMLResponse)
+def get_html() -> HTMLResponse:
+  with open("public/index.html") as html:
+    return HTMLResponse(content=html.read())
+
+@app.post("/send_message")
+async def send_message(request: Request):
+  form_data =await request.json()
+  # print(form_data)
+  # print(form_data['email'])
+  # print(type(form_data['message']))
+
+  load_dotenv("api.env")
+  sg = sendgrid.SendGridAPIClient(api_key=os.environ.get('SENDGRID_API_KEY'))
+  print(f'API Key: {os.environ.get("SENDGRID_API_KEY")}')
+
+  from_email = Email("lkimdaryl@gmail.com")
+  to_email = To("lkimdaryl@gmail.com")
+  subject = f"Message by: {form_data['name']}"
+  content = Content("text/plain", form_data['message'])
+  mail = Mail(from_email, to_email, subject, content)
+
+  try:
+    response = sg.send(mail)
+    print(response.status_code)
+    print(response.body)
+    print(response.headers)
+    return {'Status': 'Success'}
+  except Exception as e:
+    print(e)
+    return {'Status': 'Fail'}
+
+
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+# If running the server directly from Python as a module
+if __name__ == "__main__":
+  uvicorn.run(app, host="127.0.0.1", port=8000)
---
+++ public/styles/main.js
@@ -0,0 +1,79 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    setTimeout(function() {
+        var myImage = document.querySelector('#my-img');
+        myImage.classList.add('in-focus');
+    }, 100);
+
+    setTimeout(function() {
+        var myName = document.querySelector('#my-name');
+        myName.classList.add('in-focus');
+    }, 500);
+
+    setTimeout(function() {
+        var myTitle = document.querySelector('#title');
+        myTitle.classList.add('in-focus');
+    }, 1000);
+
+    setTimeout(function() {
+        var slogan = document.querySelector('#slogan');
+        slogan.classList.add('in-focus');
+    }, 1500);
+
+    var RMSBttn = document.querySelector('#RMS-bttn');
+    RMSBttn.addEventListener('click', event => {
+        window.open('https://github.com/lkimdaryl/Restaurant_Management_System', '_blank')
+    });
+
+    const form = document.querySelector("form");
+
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
+        const formData = JSON.stringify(Object.fromEntries(new FormData(form)));
+//        console.log(formData);
+        const url = form.action;
+
+        fetch(url, {
+            credentials: 'same-origin',
+            method: form.method,
+            headers: {
+                'Content-Type': 'application/json',
+                'X-Requested-With': 'XMLHttpRequest'
+            },
+            body: formData,
+        })
+        .then((response) => response.json())
+        .then((data) => {
+//            console.log(data);
+            showNotification(data['Status']);
+        })
+        .catch((error) => {
+            console.error("Error:", error);
+        });
+    });
+});
+
+function showNotification(status){
+    const form = document.getElementById("messageForm");
+    const messageTextarea = document.getElementById("message");
+    const notification = document.getElementById("notification");
+    if (status === "Success"){
+        messageTextarea.value = "";
+        messageTextarea.placeholder = "";
+        notification.textContent = "Message Sent Successfully!";
+        notification.classList.add("show-success");
+        setTimeout(function () {
+            messageTextarea.placeholder = "Enter your message here";
+            notification.classList.remove("show-success");
+        }, 2000);
+    }else{
+        messageTextarea.value = "";
+        messageTextarea.placeholder = "";
+        notification.textContent = "Sending Message Failed.";
+        notification.classList.add("show-fail");
+        setTimeout(function () {
+            messageTextarea.placeholder = "Enter your message here";
+            notification.classList.remove("show-fail");
+        }, 2000);
+    }
+}
